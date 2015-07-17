91b975a4f2ba0be59562bcab38ef53c5bd18182d
Truong Dung <truongdung@developers-iMac.local>
2015-07-17 17:38:28 +0700

    Add gem

--- server/jwt-server/app/controllers/application_controller.rb
+++ server/jwt-server/app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  before_action :authenticate_request
+
+  attr_reader :current_user
+  helper_method :current_user
+
+  private
+
+  def authenticate_request
+    @current_user = AuthenticateApiRequest.call(request.headers).result
+
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
 end
--- server/jwt-server/config/application.rb
+++ server/jwt-server/config/application.rb
@@ -22,5 +22,10 @@ module JwtServer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.generators do |generate|
+      # DISABLE ASSET GENERATORS
+      generate.javascript_engine false
+      # more generator configs...
+    end
   end
 end
